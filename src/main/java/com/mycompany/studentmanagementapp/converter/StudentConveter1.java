83752128f4419730b66a5ef92f9098c122d07890
Shubham Ajabe <66984159+shubham-ajabe@users.noreply.github.com>
2023-07-24 23:48:03 +0530

    Add files via upload

---
+++ src/main/java/com/mycompany/studentmanagementapp/converter/StudentConveter1.java
@@ -0,0 +1,23 @@
+package com.mycompany.studentmanagementapp.converter;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Component
+public class StudentConveter1 {
+    @Autowired
+    ModelMapper modelMapper;
+
+    public <S, T> T convert(S source, Class<T> targetClass) {
+        return modelMapper.map(source, targetClass);
+    }
+
+    public <S, T> List<T> mapList(List<S> source, Class<T> targetClass) {
+        return source.stream().map(element -> modelMapper.map(element, targetClass)).collect(Collectors.toList());
+    }
+}
+
