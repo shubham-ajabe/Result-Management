26338c3d998f2b756845f7835c4c0dded00b2d9e
Shubham Ajabe <66984159+shubham-ajabe@users.noreply.github.com>
2023-07-24 23:44:44 +0530

    Add files via upload

---
+++ src/main/java/com/mycompany/studentmanagementapp/constant/ErrorType.java
@@ -0,0 +1,9 @@
+package com.mycompany.studentmanagementapp.constant;
+
+public enum ErrorType {
+    AUTH_INVALID_CREDENTIALS,
+    NOT_EMPTY,
+    ALREADY_EXIT,
+    NOT_EXIT,
+    UNKNOWN_SERVER_ERROR
+}
---
+++ src/main/java/com/mycompany/studentmanagementapp/constant/Gender.java
@@ -0,0 +1,10 @@
+package com.mycompany.studentmanagementapp.constant;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum Gender{
+       MALE, FEMALE
+}
