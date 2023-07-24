4a6be0017e4cd42c928f13117f2779a99ab0c32f
Shubham Ajabe <66984159+shubham-ajabe@users.noreply.github.com>
2023-07-24 23:43:52 +0530

    Add files via upload

---
+++ src/main/java/com/mycompany/studentmanagementapp/config/AppConfig.java
@@ -0,0 +1,14 @@
+package com.mycompany.studentmanagementapp.config;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig {
+
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
+}
