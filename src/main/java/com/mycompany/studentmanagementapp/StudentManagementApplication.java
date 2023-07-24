ef512afe213c0b93d247ba6f83094ccc54a6b8b0
Shubham Ajabe <66984159+shubham-ajabe@users.noreply.github.com>
2023-07-24 23:55:06 +0530

    Add files via upload

---
+++ src/main/java/com/mycompany/studentmanagementapp/StudentManagementApplication.java
@@ -0,0 +1,14 @@
+package com.mycompany.studentmanagementapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+public class StudentManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentManagementApplication.class, args);
+	}
+
+}
